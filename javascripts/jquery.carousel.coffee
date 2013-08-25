39bfdc8455f643d7fdef90427eefac38f3f47ba2
Kevin Sylvestre <kevin@ksylvest.com>
2013-08-25 12:03:06 -0700

    added a fix for IE

--- javascripts/jquery.carousel.coffee
+++ javascripts/jquery.carousel.coffee
@@ -1,7 +1,7 @@
 ###
 jQuery Carousel
 Copyright 2010 - 2013 Kevin Sylvestre
-1.1.1
+1.1.2
 ###
 
 "use strict"
@@ -12,7 +12,6 @@ class Animation
   @transitions:
     "webkitTransition": "webkitTransitionEnd"
     "mozTransition": "mozTransitionEnd"
-    "msTransition": "msTransitionEnd"
     "oTransition": "oTransitionEnd"
     "transition": "transitionend"
 
--- javascripts/jquery.carousel.js
+++ javascripts/jquery.carousel.js
@@ -2,7 +2,7 @@
 /*
 jQuery Carousel
 Copyright 2010 - 2013 Kevin Sylvestre
-1.1.1
+1.1.2
 */
 
 
@@ -18,7 +18,6 @@ Copyright 2010 - 2013 Kevin Sylvestre
     Animation.transitions = {
       "webkitTransition": "webkitTransitionEnd",
       "mozTransition": "mozTransitionEnd",
-      "msTransition": "msTransitionEnd",
       "oTransition": "oTransitionEnd",
       "transition": "transitionend"
     };
